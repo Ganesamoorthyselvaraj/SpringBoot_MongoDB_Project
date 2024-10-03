c344e829f01401b2825fe5d9f70288427847a240
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-10-03 13:48:39 +0530

    Update User.java

--- customer-service/src/main/java/com/example/userservice/model/User.java
+++ customer-service/src/main/java/com/example/userservice/model/User.java
@@ -3,7 +3,7 @@ package com.example.userservice.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
+@Document(collection = "customer")
 public class User {
     @Id
     private String id;
