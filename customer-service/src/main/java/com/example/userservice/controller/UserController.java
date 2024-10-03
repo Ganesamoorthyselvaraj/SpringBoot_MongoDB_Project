0ebcfd221523ae274725c66a0fe852882a44c5b2
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-10-03 13:57:29 +0530

    Update UserController.java

--- customer-service/src/main/java/com/example/userservice/controller/UserController.java
+++ customer-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/customer")
 public class UserController {
 
     @Autowired
